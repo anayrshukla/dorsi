27c0710e0db2d62724da50b2e67cc5eda6d9c1b0
anayrshukla <anayrshukla@gmail.com>
2024-10-15 17:46:14 -0700

    second commit

--- d1/next.config.mjs
+++ d1/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    trailingSlash: true,
+    assetPrefix: '',  // Use a leading slash for asset prefix
+    output: 'export',
+  };
+  
+  export default nextConfig;
+  
